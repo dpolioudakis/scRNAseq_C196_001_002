2a7a2ef6500902756f9cae5b064664ce0ee4e312
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2016-02-18 18:39:06 -0800

    Completed script

--- Compile_metadata.R
+++ Compile_metadata.R
@@ -1,9 +1,18 @@
 
+# Damon Polioudakis
+# 2016-02-18
+# Add some RNA STAR statistics and Picard Statistics to sample metadata
+# Gene Length and GC bias were calculated with Calc_Gene_Length_and_GC.R
+################################################################################
 
 rm(list=ls())
 
+library(xlsx)
 
+# Load data and assign variables
 
+# Picard Sequencing Statistics
+picStatsDF <- read.csv("../metadata/PicardToolsQC.csv")
 
 # RNA STAR Stats for Run 1 
 stStats1dF <- read.table("../metadata/RNAstar_Stats_SxaQSEQsXap089L2.txt"
@@ -14,26 +23,73 @@ stStats2dF <- read.table("../metadata/RNAstar_Stats_SxaQSEQsXbp060L2.txt"
 
 # Metadata
 metDatDF <- read.xlsx("../metadata/PercentageReadsMapping.xlsx", 1)
-pctDupDF <- read.xlsx("../metadata/PercentDuplicates.xlsx", 1)
 libBatch1DF <- read.xlsx("../metadata/LibraryBatch_Well_ID.xlsx", 1, header = FALSE)
 libBatch2DF <- read.xlsx("../metadata/LibraryBatch_Well_ID.xlsx", 2, header = FALSE)
 
 # GC content and average length for each sample
-load("../analysis/tables/Gene_Length_GC_Bias.rda")
-lenBiasDF <- data.frame(GeneLengthScore = lenBias)
-gcBiasDF <- data.frame(GCcontent = gcBias)
+load("../analysis/tables/Avg_Gene_Length_and_GC.rda")
+avgLengthDF <- data.frame(AvgGeneLength = avgLength)
+avgGCdF <- data.frame(GCcontent = avgGC)
 ################################################################################
 
 ### Format data and add to metadata
 
 ##
-# And add percent duplication data to metadata table
+# Remove statistics from Run 1 from metadata
+metDatDF <- subset(metDatDF, select = -c(NA., uniquelymappedreads
+                              , mappedtomultipleloci, NumReads))
 
-# Edit CellIDs to be same format
-pctDupDF <- data.frame(PERCENT_DUPLICATION = t(pctDupDF)[-1, 1])
-row.names(pctDupDF) <- gsub("\\..*\\.txt", "", row.names(pctDupDF))
+##
+# Calculate and add percent duplication data to metadata table
+
+# Calculate percent duplication
+pctDupDF <- data.frame(Pct_Duplicates =
+                  picStatsDF$READ_PAIR_DUPLICATES / picStatsDF$PF_READS_ALIGNED
+                  , row.names = picStatsDF$X)
 # And add percent duplication data to metadata table
 metDatDF <- merge(x = metDatDF, y = pctDupDF, by.x = "CellID", by.y = "row.names")
+
+##
+# Calculate and add total reads, percent uniquely mapped reads, percent mapped
+# to multiple loci, and percent unmapped reads to metadata table
+
+# Total Reads
+totReads <- (stStats1dF$Number.of.input.reads
+                + stStats2dF$Number.of.input.reads)
+metDatDF$Total_Reads <- totReads
+
+# Uniquely Mapped - Convert % to numeric and multiply by number of reads in lane
+umReads <- (((as.numeric(sub("%", "", stStats1dF$Uniquely.mapped.reads..))/100) * stStats1dF$Number.of.input.reads)
+         + (((as.numeric(sub("%", "", stStats2dF$Uniquely.mapped.reads..))/100) * stStats2dF$Number.of.input.reads)))
+# Percent Uniquely Mapped
+pctUMRds <- umReads / totReads
+metDatDF$Pct_Uniquely_Mapped <- pctUMRds
+
+# Total mapped to multiple loci
+totMpMtLoci <- (stStats1dF$Number.of.reads.mapped.to.multiple.loci
+              + stStats2dF$Number.of.reads.mapped.to.multiple.loci)
+# Percent mapped to multiple loci
+pctMpMtLoci <- totMpMtLoci / totReads
+metDatDF$Pct_Mapped_Multiple_Loci <- pctMpMtLoci
+
+# Percent unmapped reads
+Calc_Unmapped <- function (data) {
+  # Convert % to numeric and multiply by number of reads in lane
+  df <- apply(data[ ,c("X..of.reads.unmapped..too.many.mismatches"
+                                      , "X..of.reads.unmapped..too.short"
+                                      , "X..of.reads.unmapped..other")]
+              , 2, function(column) {as.numeric(sub("%", "", column)) / 100})
+  # Multiple by number of reads in lane to convert to read number
+  df <- apply(df, 2, function(column) {column * data$Number.of.input.reads})
+  apply(df, 1, sum)
+  }
+# Lane 1
+unMap1 <- Calc_Unmapped(stStats1dF)
+# Lane 2
+unMap2 <- Calc_Unmapped(stStats2dF)
+# Percent unmapped reads
+pctUnMap <- (unMap1 + unMap2) / totReads
+metDatDF$Pct_Unmapped <- pctUnMap
 
 ##
 # Add batch ID to metadata
@@ -50,17 +106,24 @@ metDatDF <- merge(x = metDatDF, y = batchIDdF,
                   by.x = "CaptureWellID", by.y = "CaptureWellID")
 
 ##
-# Add length bias to metadata
+# Add average gene length to metadata
 
-row.names(lenBiasDF) <- gsub("_e.*", "", row.names(lenBiasDF))
-lenBiasDF$CellID <- gsub("_e.*", "", row.names(lenBiasDF))
-lenBiasDF <- lenBiasDF[2:(nrow(lenBiasDF)-1), ]
-metDatDF <- merge(x = metDatDF, y = lenBiasDF, by.x = "CellID", by.y = "CellID")
+row.names(avgLengthDF) <- gsub("_e.*", "", row.names(avgLengthDF))
+avgLengthDF$CellID <- gsub("_e.*", "", row.names(avgLengthDF))
+avgLengthDF <- avgLengthDF[2:(nrow(avgLengthDF)-1), ]
+metDatDF <- merge(x = metDatDF, y = avgLengthDF, by.x = "CellID", by.y = "CellID")
 
 ##
-# Add GC bias to metadata
+# Add average GC content to metadata
 
-row.names(gcBiasDF) <- gsub("_e.*", "", row.names(gcBiasDF))
-gcBiasDF$CellID <- gsub("_e.*", "", row.names(gcBiasDF))
-gcBiasDF <- gcBiasDF[2:(nrow(gcBiasDF)-1), ]
-metDatDF <- merge(x = metDatDF, y = gcBiasDF, by.x = "CellID", by.y = "CellID")
+row.names(avgGCdF) <- gsub("_e.*", "", row.names(avgGCdF))
+avgGCdF$CellID <- gsub("_e.*", "", row.names(avgGCdF))
+avgGCdF <- avgGCdF[2:(nrow(avgGCdF)-1), ]
+metDatDF <- merge(x = metDatDF, y = avgGCdF, by.x = "CellID", by.y = "CellID")
+
+##
+# Write out to tab separated table
+head(metDatDF)
+write.table(metDatDF, file = paste0("../metadata/Compiled_Metadata_"
+                                , format(Sys.time(), "%Y%m%d"), ".txt")
+          , quote = FALSE, row.names = FALSE, sep = "\t")
