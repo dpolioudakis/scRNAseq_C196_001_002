c69a8caf18785309e800a22f1caec224d93c768b
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2016-03-22 17:25:47 -0700

    Added ERCC plots

--- Determine_Usable_Cells.R
+++ Determine_Usable_Cells.R
@@ -19,16 +19,20 @@ library(ggplot2)
 picStatsDF <- read.csv("../metadata/PicardToolsQC.csv")
 
 # Metadata
-metDatDF <- read.table("../metadata/combined_DP_2016-01-28.txt", header = TRUE)
+metDatDF <- read.table("../metadata/Compiled_Metadata_20160218.txt"
+                       , header = TRUE, sep = "\t")
 
 exDatDF <- read.csv("../data/htseq/merged/Exprs_HTSCexon.csv", row.names = 1)
 
 colnames(picStatsDF)
 
+metDatDF <- metDatDF[order(as.factor(metDatDF$CellID)), ]
+metDatDF$CellID
+
 exDatDF <- exDatDF[ ,order(colnames(exDatDF))]
 colnames(exDatDF)
 
-picStatsDF <- picStatsDF[order(picStatsDF$X), ]
+wpicStatsDF <- picStatsDF[order(picStatsDF$X), ]
 picStatsDF$X
 
 # pctUM <- picStatsDF$PF_READS_ALIGNED/picStatsDF$TOTAL_READS
@@ -38,4 +42,7 @@ nMapERCC <- apply(head(tail(exDatDF, 97), -5), 2, sum)
 pctERCC <- nMapERCC/picStatsDF$PF_READS_ALIGNED
 round(pctERCC, 2)
 
-plot(picStatsDF$PF_READS_ALIGNED/(10^6), pctERCC)
+plot(picStatsDF$PF_READS_ALIGNED/(10^6), pctERCC, col = as.factor(metDatDF$VisualQC))
+
+plot(picStatsDF$PF_READS_ALIGNED/(10^6), (1 -metDatDF$Pct_Uniquely_Mapped)
+     , col = as.factor(metDatDF$VisualQC))
